f0c3ac4a788a517c2f09a5d2fb88a754b57757a9
James Jarvis <jamespjarvis@gmail.com>
2017-01-24 12:30:01 -0500

    /users responds with user list

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/User');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    User.find({}, (err, users) => {
+        if (err) return next(err);
+        return res.send(users);
+    });
 });
 
 module.exports = router;
