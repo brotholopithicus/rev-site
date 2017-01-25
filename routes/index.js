b49da67aa622a5d549ddf638cf3cb53eb082cc55
James Jarvis <jamespjarvis@gmail.com>
2017-01-25 09:26:19 -0500

    added some routes, including exploit page

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,26 @@ router.get('/', (req, res, next) => {
     res.render('index', { title: 'Home' });
 });
 
+/* GET login page. */
+router.get('/login', (req, res, next) => {
+    res.send('LOGIN PAGE');
+});
+
+/* GET signup page. */
+router.get('/signup', (req, res, next) => {
+    res.send('SIGNUP PAGE');
+});
+
+/* GET exploits page. */
+router.get('/exploits', (req, res, next) => {
+    res.render('exploits');
+});
+
+/* GET legal page. */
+router.get('/legal', (req, res, next) => {
+    res.render('legal');
+});
+
 /* GET shop page. */
 router.get('/shop', (req, res, next) => {
     Product.find({}, (err, products) => {
