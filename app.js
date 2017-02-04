ff85eabdc7e771b8f77bad9752b6e995bccaef9a
James Jarvis <jamespjarvis@gmail.com>
2017-02-04 10:03:53 -0500

    passport auth

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use((req, res, next) => {
     res.locals.user = req.session.user;
     next();
 });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
