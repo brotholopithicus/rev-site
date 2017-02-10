dec2b180fd8281e2c3eee4f836c5a0adffdfc234
James Jarvis <jamespjarvis@gmail.com>
2017-02-10 10:05:18 -0500

    reformatted map scripts

--- public/js/map.js
+++ public/js/map.js
@@ -1,73 +1,86 @@
-navigator.geolocation.getCurrentPosition(success, error);
 const locationList = document.querySelector('.location-list');
-const map = L.map('map').setView([39, -98], 4);
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnJvdGhvbG9waXRoaWN1cyIsImEiOiJjaXVoM293am8wMHQ0M3BxZm1lMDV6cWN4In0.akjY7P_g6dfE8UZiZoK9Ag').addTo(map);
+
+navigator.geolocation.getCurrentPosition(success, error);
 
 function success(pos) {
-    let latitude = pos.coords.latitude;
-    let longitude = pos.coords.longitude;
-    getSortedLocations({ latitude, longitude });
-    localStorage.setItem('location', JSON.stringify({ latitude, longitude }));
-    map.panTo([latitude, longitude]);
+    let userCoords = {
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude
+    }
     let homeIcon = L.icon({
         iconUrl: '../img/house.png',
-        iconSize: [32, 32],
-        iconAnchor: [16, 16],
-        popupAnchor: [-1, -16]
-    })
-    let currentLocation = L.marker([latitude, longitude], { icon: homeIcon }).addTo(map).bindPopup('<b>Current Location</b>').openPopup();
+        iconSize: [24, 24],
+        iconAnchor: [12, 0]
+    });
+
+    let userLocation = L.marker([userCoords.latitude, userCoords.longitude], { icon: homeIcon }).addTo(map).bindPopup('<h6>Current Location</h6>');
+
+    sortLocationsByProximity(userCoords)
+        .then(
+            (response) => {
+                updateLocationListView(response);
+            },
+            (error) => {
+                console.log(error);
+            });
+
 }
 
-function storeController(stores) {
-    stores = stores || JSON.parse(localStorage.getItem('stores'));
+function error(err) {
+
+}
+
+function updateLocationListView(stores) {
+    let spinner = document.querySelector('div.fading-circle');
+    locationList.removeChild(spinner);
+    let storeIcon = L.icon({
+        iconUrl: '../img/logo.svg',
+        iconSize: [24, 24],
+        iconAnchor: [12, 0]
+    });
+
     stores.forEach(store => {
-        let url = `https://www.google.com/maps/dir//${encodeURIComponent(store.address)}`
-        let storeMarker = L
-            .marker([store.coords.latitude, store.coords.longitude])
+        let url = `https://www.google.com/maps/dir//${encodeURIComponent(store.address)}`;
+        let storeMarker = L.marker([store.coords.latitude, store.coords.longitude], { icon: storeIcon })
             .addTo(map)
-            .bindPopup(`<b>${store.name.toUpperCase()}</b><br />${store.address}<br />${store.distance} miles<br /><a href='${url}'>Directions</a>`);
-        let data = { store, storeMarker };
-        storeMarker.addEventListener('click', storeClickHandler.bind(data));
+            .bindPopup(
+                `<b>${store.name.toUpperCase()}</b><br />
+              ${store.address}<br />
+              ${store.distance} miles<br />
+              <a href='${url}'>Directions</a>`
+            );
         let el = document.createElement('li');
         el.classList.add('storeLocation');
-        el.dataset.latitude = store.coords.latitude;
-        el.dataset.longitude = store.coords.longitude;
         el.innerHTML = `<div><span class='name'>${store.name}</span><br />${store.address}<br />${parseFloat(store.distance).toFixed(1)} miles<br /></div><div><a class='directions' href='${url}'>Directions</a></div>`;
-        el.addEventListener('click', storeClickHandler.bind(data));
+
+        el.addEventListener('click', locationClickHandler.bind(storeMarker));
         locationList.appendChild(el);
     });
 }
 
-let polyline;
+const map = L.map('map').setView([39, -98], 4);
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnJvdGhvbG9waXRoaWN1cyIsImEiOiJjaXVoM293am8wMHQ0M3BxZm1lMDV6cWN4In0.akjY7P_g6dfE8UZiZoK9Ag').addTo(map);
 
-function storeClickHandler(e) {
-    if (e.target.tagName === 'A') return;
-    let userCoords = JSON.parse(localStorage.getItem('location'));
-    let arr = [
-        [userCoords.latitude, userCoords.longitude],
-        [this.store.coords.latitude, this.store.coords.longitude]
-    ];
-    if (polyline) {
-        polyline.removeFrom(map);
-    }
-    polyline = L.polyline(arr, { color: 'red' }).addTo(map);
-    this.storeMarker.openPopup();
-    map.fitBounds(polyline.getBounds());
-}
-
-function error() {
-    console.log('error');
-}
-
-function getSortedLocations(coords) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/api/locations');
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let data = JSON.parse(xhr.responseText);
-            storeController(data);
+function sortLocationsByProximity(coords) {
+    return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        request.open('POST', '/api/locations');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.responseType = 'json';
+        request.onload = () => {
+            if (request.status === 200) {
+                resolve(request.response);
+            } else {
+                reject(Error(`Location Data Didn't Load Correctly. Code: ${request.statusText}`));
+            }
         }
-    }
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(coords));
+        request.onerror = () => reject(Error('Network Error'));
+        request.send(JSON.stringify(coords));
+    });
+}
+
+function locationClickHandler(e) {
+    if (e.target.tagName === 'A') return;
+    map.setView([this._latlng.lat, this._latlng.lng], 4, { animate: true, duration: 0.4 });
+    this.openPopup();
 }
